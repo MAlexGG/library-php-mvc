9564f9138c8c3f1ce60f2a83884782e4c53162d2
MAGG <alexandragalarzag@gmail.com>
2023-01-18 19:12:06 +0100

    navbar, create form and bootstrap styles

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("./view/head/head.php");
 ?>
-
+<a href="/library-php-mvc/view/book/create.php" class="btn btn-dark">Agregar Libro</a>
 
 
 
---
+++ view/book/create.php
@@ -0,0 +1,31 @@
+<?php
+require_once("C://xampp/htdocs/library-php-mvc/view/head/head.php");
+?>
+
+<form action="store.php" method="POST">
+    <div class="mb-3">
+        <label for="isbn" class="form-label">ISBN</label>
+        <input type="text" name="isbn" class="form-control" id="isbn" aria-describedby="isbn">
+    </div>
+    <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" name="title" class="form-control" id="title" aria-describedby="title">
+    </div>
+    <div class="mb-3">
+        <label for="author" class="form-label">Author</label>
+        <input type="text" name="author" class="form-control" id="author" aria-describedby="author">
+    </div>
+    <div class="mb-3">
+        <label for="year" class="form-label">Year</label>
+        <input type="text" name="year" class="form-control" id="year" aria-describedby="year">
+    </div>
+
+    <button type="submit" class="btn btn-dark">Guardar</button>
+    <a class="btn btn-warning" href="../../index.php">Cancelar</a>
+</form>
+
+
+
+<?php
+require_once("C://xampp/htdocs/library-php-mvc/view/head/footer.php");
+?>
