24dbda073ae4b082fce9eefd7a0c4958e72ce197
MAGG <alexandragalarzag@gmail.com>
2023-01-22 20:10:07 +0100

    modal outside the card

--- view/book/index.php
+++ view/book/index.php
@@ -26,27 +26,30 @@ $books = $obj->getBooks();
                             <a href="show.php?id=<?= $book["id"] ?>" class="btn btn-dark">Read more...</a>
                             <a href="edit.php?id=<?= $book["id"] ?>" class="btn btn-warning">Edit</a>
                             <a class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal-<?= $book["id"] ?>">Delete</a>
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal-<?= $book["id"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Do you want to delete this book?</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            You will delete the book <strong><?= $book["title"] ?></strong> and you will not be able to undo this operation
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel</button>
-                                            <a href="delete.php?id=<?= $book["id"] ?> " class="btn btn-danger">Delete</a>
-                                        </div>
-                                    </div>
-                                </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal-<?= $book["id"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Do you want to delete this book?</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                You will delete the book <strong><?= $book["title"] ?></strong> and you will not be able to undo this operation
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel</button>
+                                <a href="delete.php?id=<?= $book["id"] ?> " class="btn btn-danger">Delete</a>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+
             <?php endforeach; ?>
         </div>
 
