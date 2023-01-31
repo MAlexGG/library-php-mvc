2a84edabb302ee3030be7981742c1194d62f7038
MAGG <alexandragalarzag@gmail.com>
2023-01-31 10:06:04 +0100

    body without overflow hidden

--- view/head/header.php
+++ view/head/header.php
@@ -10,9 +10,13 @@
             ?>
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="./resources/css/styles.css">
+    <link rel="stylesheet" href="http://localhost/library-php-mvc/resources/css/styles.css">
+    <link rel="stylesheet" href="http://localhost/library-php-mvc/resources/css/styles-books.css">
+    <link rel="stylesheet" href="http://localhost/library-php-mvc/resources/css/styles-book.css">
+
+    <!-- <link rel="stylesheet" href="./resources/css/styles.css">
     <link rel="stylesheet" href="../../resources/css/styles-books.css">
-    <link rel="stylesheet" href="../../resources/css/styles-book.css">
+    <link rel="stylesheet" href="../../resources/css/styles-book.css"> -->
 </head>
 
 <body class="container-fluid p-0 ct-body">
